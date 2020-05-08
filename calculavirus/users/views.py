00ef2f393c7f87a688ddb83c34c1801b37b816da
Martin Helmut Dominguez Alvarez <martinhelmutmh@gmail.com>
2020-05-08 14:56:14 -0500

    API listo

---
+++ calculavirus/users/views.py
@@ -0,0 +1,33 @@
+from django.contrib.auth.models import User, Group
+from rest_framework import viewsets
+from rest_framework import permissions
+from calculavirus.users.serializers import UserSerializer, GroupSerializer
+
+
+'''
+Usar un Auth Sencillo
+Enviar los parámetros de registro en el Body
+
+curl -H 'Accept: application/json; indent=4' -u admin:password123 http://127.0.0.1:8000/users/
+
+'''
+
+
+
+class UserViewSet(viewsets.ModelViewSet):
+    """
+    API endpoint that allows users to be viewed or edited.
+    """
+    queryset = User.objects.all().order_by('-date_joined')
+    serializer_class = UserSerializer
+    permission_classes = [permissions.IsAuthenticated]
+    
+
+
+class GroupViewSet(viewsets.ModelViewSet):
+    """
+    API endpoint that allows groups to be viewed or edited.
+    """
+    queryset = Group.objects.all()
+    serializer_class = GroupSerializer
+    permission_classes = [permissions.IsAuthenticated]
