0f451d67fa963915e731620b4ebe3951a7d36b82
Martin Helmut Dominguez Alvarez <martinhelmutmh@gmail.com>
2020-05-21 15:00:23 -0500

    More images

--- calculavirus/insumos/models.py
+++ calculavirus/insumos/models.py
@@ -23,7 +23,7 @@ class Insumo(models.Model):
     nombre = models.CharField( max_length=120)
     marca = models.CharField( max_length=120)
     descripcion = models.TextField(blank=True)
-    lugar_compra = models.OneToOneField(
+    lugar_compra = models.ForeignKey(
         LugarCompra,
         on_delete=models.CASCADE,
     )
