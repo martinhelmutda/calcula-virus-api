590278f0e6aa6997fb56aa27916de865f307dc0b
Martin Helmut Dominguez Alvarez <martinhelmutmh@gmail.com>
2020-05-30 15:34:01 -0500

    Simple user-email field

--- calculavirus/checklist/models.py
+++ calculavirus/checklist/models.py
@@ -4,11 +4,11 @@ from calculavirus.customeUsers.models import CustomUsers
 
 class Checklist(models.Model):
     lugar_compra = models.CharField( max_length=120)
-    user = models.ForeignKey(CustomUsers, on_delete=models.CASCADE,null=True)
+    user = models.CharField( max_length=120)
 
 class ChecklistInsumo(models.Model):
     insumo=models.ForeignKey(Insumo,on_delete=models.CASCADE)
     checklist=models.ForeignKey(Checklist,on_delete=models.CASCADE)
-    user = models.ForeignKey(CustomUsers, on_delete = models.CASCADE)
+    user = models.CharField( max_length=120)
     cantidad = models.IntegerField()
     comprado = models.BooleanField()
